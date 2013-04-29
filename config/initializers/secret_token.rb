7c671c3224b551bd35c8a76b7a752e3bda9ac0b0
Alex Litvinok <litvinok@gmail.com>
2013-04-29 22:26:22 +0300

    set enviroment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bodyclock::Application.config.secret_token = 'a9e284f2789616ad15ca2f1a250a071a1f00fc784ae84cb1cf447a6b1af39fa74ee60cf8a17e0b500f962c56b9403aeb26de0f15587dcc4f13b0f61f5d47df2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bodyclock::Application.config.session_store :cookie_store, key: '_bodyclock_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bodyclock::Application.config.session_store :active_record_store
