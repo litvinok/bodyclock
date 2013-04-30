87fedff90d09edf00f4219af611b331af3915dd8
Alex Litvinok <litvinok@gmail.com>
2013-04-30 15:06:01 +0300

    ruby 2.0.0

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
     user = User.find_or_initialize_by( :uid => auth[:uid], :provider => auth[:provider] )
 
-    user.refresh_token = auth["credentials"]["refresh_token"] if auth["credentials"]["refresh_token"]
+    user.refresh_token = auth["credentials"]["refresh_token"]
     user.access_token = auth["credentials"]["token"]
     user.expires = auth["credentials"]["expires_at"]
     user.name = auth["info"]["name"]
