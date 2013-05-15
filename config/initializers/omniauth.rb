b1fb71febf703cf250169be694e63d8f195c71e7
Alex Litvinok <litvinok@gmail.com>
2013-05-15 15:25:43 +0300

    adjust options

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   AUTH_CONFIG.each do | type, params |
 
+        puts params
+
         provider type, params[ "app_id" ], params[ "secret" ], params[ "options" ] || {}
 
   end
