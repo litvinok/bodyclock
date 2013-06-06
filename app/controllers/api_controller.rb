77b928559cb0fcbccd0a04e4ccb021dfd0608cb2
Alex Litvinok <litvinok@gmail.com>
2013-06-06 05:57:14 +0300

    fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -12,7 +12,7 @@ class ApiController < ApplicationController
     respond_to do |format|
       format.html { render }
       format.json { render :json => @info }
-      format.xml  { render :xml => @info }
+      format.xml { render :xml => @info }
     end
 
   end
@@ -20,8 +20,8 @@ class ApiController < ApplicationController
   def between
 
     offset = 2.month
-    from = params[:from] ? Date.parse( params[:from] ) : Time.now - offset
-    to =  params[:to] ? Date.parse( params[:to] ) : Time.now + offset
+    from = params[:from] ? Date.parse(params[:from]) : Time.now - offset
+    to = params[:to] ? Date.parse(params[:to]) : Time.now + offset
 
     @items = Event.where(
         :date.gt => from,
@@ -32,7 +32,7 @@ class ApiController < ApplicationController
     respond_to do |format|
 
       format.json { render :json => @items }
-      format.xml  { render :xml => @items }
+      format.xml { render :xml => @items }
     end
 
     #@date = Date.new( params[:year].to_i, params[:month].to_i, params[:day].to_i )
@@ -44,14 +44,14 @@ class ApiController < ApplicationController
   def events
 
     @items = Event.where(
-        :date => Date.parse( params[:date] ),
+        :date => Date.parse(params[:date]),
         :user => session[:user_id]
     )
 
     respond_to do |format|
 
       format.json { render :json => @items }
-      format.xml  { render :xml => @items }
+      format.xml { render :xml => @items }
     end
 
   end
